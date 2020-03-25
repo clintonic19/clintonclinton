97f8699ebe7549267a96b409ba91c9595ebc1967
clintonic19 <44939329+clintonic19@users.noreply.github.com>
2020-03-25 12:27:31 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+// stored name in variable
+
+let message;
+
+message = 'Clinton'; 
+
+// stored courses in array
+
+var courses = [
+"HTML", "CSS", "JAVASCRIPTS" ]
+
+console.log(message);
+console.log (courses);
+
+//displaying odd number between 1 and 200 
+
+for(var n = 2; n < 200; n++)
+{
+ if( n % 2==1)
+ 	{ console.log (n + " is odd") }
+ 
+ }
+
+
